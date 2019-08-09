d9888766a375a0ae1f0ef4942e7a9d2427049ac7
Camilla <cgalloni@cern.ch>
2019-08-09 15:42:57 +0200

    adding files and instructions in README to run 10M evts for BJpsiX

--- 2018/run_crab_AOD_prod.sh
+++ 2018/run_crab_AOD_prod.sh
@@ -7,22 +7,10 @@ echo "================= CMSRUN starting jobNum=$1 ====================" | tee -a
 
 echo "================= CMSRUN cmsenv CMSSW_10_2_9 ===================="| tee -a job.log
 
-#mkdir -p Configuration/GenProduction/python
-#cp -v $BASE/LHE_crab_cfg.py Configuration/GenProduction/python/
-#cp -v $BASE/GS_Fall18_BcJpsiMuNu_crab_cfg.py Configuration/GenProduction/python/
 
-
-#scram b
-#cd $BASE
-
-#echo "================= CMSRUN starting LHE 1 ====================" | tee -a job.log
-
-#cmsRun -j LHE_step1.log  LHE_crab_cfg.py jobNum=$1
-
-#cd $BASE
 echo "================= CMSRUN starting GS 2 ====================" | tee -a job.log
 
-cmsRun -j GS_step2.log MUO-RunIIFall17GS-0001_2018_crab_cfg.py jobNum=$1 #
+cmsRun -j GS_step2.log BPH-BJpsiX_MuMu_2018_cfg.py jobNum=$1 #
 #cmsRun -j GS_step2.log JME-RunIIFall18GS-00009_crab_cfg.py jobNum=$1
 echo "-> cleaning"
 #rm -v lhe1.root  
