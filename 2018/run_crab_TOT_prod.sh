1d5ae964f8a1f621eb9e32233f3b2523f3c29690
Federica Riti <federica.riti@cern.ch>
2020-07-15 14:10:24 +0200

    clear files

--- 2018/run_crab_TOT_prod.sh
+++ 2018/run_crab_TOT_prod.sh
@@ -13,37 +13,37 @@ echo "================= CMSRUN cmsenv CMSSW_10_2_9 ===================="| tee -a
 #cmsRun -j GS_step2.log BPH-JpsiX_MuMu_2018_cfg_Jordan.py jobNum=$1 #
 #cmsRun -j GS_step2.log JME-RunIIFall18GS-00009_crab_cfg.py jobNum=$1
 #echo "-> cleaning"
-#rm -v lhe1.root  
+#rm -v BTV-RunIISummer19UL18SIM-00025.root  
 
 echo "================= CMSRUN starting SIM ====================" | tee -a job.log
 
 cmsRun -j DR_SIM.log  BTV-RunIISummer19UL18SIM-00025_1_cfg.py #DR_step1_crab_cfg_ac.py 
-#echo "-> cleaning"
-#rm -v GS1.root 
+echo "-> cleaning"
+rm -v BTV-RunIISummer19UL18SIM-00025.root  
 
 
 echo "================= CMSRUN starting PREMIX ====================" | tee -a job.log
 
 cmsRun -j  DR_Premix.log  BTV-RunIISummer19UL18DIGIPremix-00025_1_cfg.py #DR_step2_crab_cfg.py 
-#echo "-> cleaning"
-#rm -v DR_step1.root
+echo "-> cleaning"
+rm -v BTV-RunIISummer19UL18DIGIPremix-00025.root
 
 echo "================= CMSRUN starting HLT ====================" | tee -a job.log
 
 cmsRun -j  DR_HLT.log  BTV-RunIISummer19UL18HLT-00025_1_cfg.py
-#echo "-> cleaning"
-#rm -v DR_step1.root
+echo "-> cleaning"
+rm -v BTV-RunIISummer19UL18HLT-00025.root
 
 echo "================= CMSRUN starting RECO ====================" | tee -a job.log
 
 cmsRun -j  DR_RECO.log  BTV-RunIISummer19UL18RECO-00025_1_cfg.py
-#echo "-> cleaning"
-#rm -v DR_step1.root
+echo "-> cleaning"
+rm -v BTV-RunIISummer19UL18RECO-00025.root
 
 
 echo "================= CMSRUN  min ===================="  | tee -a job.log
 
 cmsRun -e -j FrameworkJobReport.xml BTV-RunIISummer19UL18MiniAOD-00025_1_cfg.py
-#rm -v fileAOD.root
+rm -v BTV-RunIISummer19UL18MiniAOD-00025.root
 
 echo "================= CMSRUN  finished ===================="  | tee -a job.log
