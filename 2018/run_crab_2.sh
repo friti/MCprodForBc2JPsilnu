3de56bb98f37280794bcae07355d5c61da2a8b40
Federica Riti <federica.riti@cern.ch>
2020-07-20 14:05:07 +0200

    mod

--- 2018/run_crab_2.sh
+++ 2018/run_crab_2.sh
@@ -2,7 +2,8 @@
 
 echo "================= CMSRUN starting HLT ====================" | tee -a job.log
 
-cmsRun -j  DR_HLT.log  BTV-RunIISummer19UL18HLT-00025_1_cfg.py
-#echo "-> cleaning"
-#rm -v BTV-RunIISummer19UL18DIGIPremix-00025.root
+#cmsRun -j  DR_HLT.log  BTV-RunIISummer19UL18HLT-00025_1_cfg.py
 
+cmsRun -e -j FrameworkJobReport.xml  BTV-RunIISummer19UL18HLT-00025_1_cfg.py
+echo "-> cleaning"
+rm -v BTV-RunIISummer19UL18DIGIPremix-00025.root
--- 2018/run_crab_3.sh
+++ 2018/run_crab_3.sh
@@ -2,15 +2,16 @@
 echo "================= CMSRUN starting RECO ====================" | tee -a job.log
 
 cmsRun -j  DR_RECO.log  BTV-RunIISummer19UL18RECO-00025_1_cfg.py
-#echo "-> cleaning"
-#rm -v BTV-RunIISummer19UL18HLT-00025.root
+echo "-> cleaning"
+rm -v BTV-RunIISummer19UL18HLT-00025.root
 
 
 echo "================= CMSRUN  min ===================="  | tee -a job.log
 
 cmsRun -e -j FrameworkJobReport.xml BTV-RunIISummer19UL18MiniAOD-00025_1_cfg.py
 
-#echo "-> cleaning"
-#rm -v BTV-RunIISummer19UL18RECO-00025.root
+echo "-> cleaning"
+rm -v BTV-RunIISummer19UL18RECO-00025.root
 
 echo "================= CMSRUN  finished ===================="  | tee -a job.log
+
