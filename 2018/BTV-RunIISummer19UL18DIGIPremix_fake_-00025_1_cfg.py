fedd46e814a80e7adeab7d1fa6186c01dae4409e
Federica Riti <federica.riti@cern.ch>
2020-08-12 10:20:49 +0200

    fake premix

---
+++ 2018/BTV-RunIISummer19UL18DIGIPremix_fake_-00025_1_cfg.py
@@ -0,0 +1,113 @@
+
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step1 --fileout file:BTV-RunIISummer19UL18DIGIPremix-00025.root --pileup_input dbs:/Neutrino_E-10_gun/RunIISummer19ULPrePremix-UL18_106X_upgrade2018_realistic_v11_L1v1-v2/PREMIX --mc --eventcontent PREMIXRAW --runUnscheduled --datatier GEN-SIM-DIGI --conditions 106X_upgrade2018_realistic_v11_L1v1 --step DIGI,DATAMIX,L1,DIGI2RAW --procModifiers premix_stage2 --nThreads 8 --geometry DB:Extended --datamix PreMix --era Run2_2018 --python_filename BTV-RunIISummer19UL18DIGIPremix-00025_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n 2000
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+from Configuration.ProcessModifiers.premix_stage2_cff import premix_stage2
+
+process = cms.Process('DIGI2RAW',eras.Run2_2018,premix_stage2)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.DigiDM_cff')
+process.load('Configuration.StandardSequences.DataMixerPreMix_cff')
+process.load('Configuration.StandardSequences.SimL1EmulatorDM_cff')
+process.load('Configuration.StandardSequences.DigiToRawDM_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+# Input source                                                                                                         
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step1 nevts:-1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.PREMIXRAWoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-DIGI'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:BTV-RunIISummer19UL18DIGIPremix-00025.root'),
+    outputCommands = process.PREMIXRAWEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from puUL import puListFull
+import random
+random.shuffle(puListFull)
+puList_tmp= puListFull[0:20]
+puList = []
+for i in range(len(puList_tmp)):
+    puList.append('root://cms-xrd-global.cern.ch//'+puList_tmp[i])
+
+#process.mix.digitizers = cms.PSet(process.theDigitizersMixPreMix)
+process.mixData.input.fileNames = cms.untracked.vstring(puList)
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+process.GlobalTag = GlobalTag(process.GlobalTag, '106X_upgrade2018_realistic_v11_L1v1', '')
+
+# Path and EndPath definitions
+process.digitisation_step = cms.Path(process.pdigi)
+process.datamixing_step = cms.Path(process.pdatamix)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.PREMIXRAWoutput_step = cms.EndPath(process.PREMIXRAWoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.digitisation_step,process.datamixing_step,process.L1simulation_step,process.digi2raw_step,process.endjob_step,process.PREMIXRAWoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+#process.options.numberOfThreads=cms.untracked.uint32(8)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+#do not add changes to your config after this point (unless you know what you are doing)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+process=convertToUnscheduled(process)
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
